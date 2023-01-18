604a66c514b9bef4905f055c2971a3146103d375
CodingdAwn <gaoxifengfordawn@gmail.com>
2023-01-19 00:31:17 +0800

    some comment

--- lua/dawn/default_plugins/lsp.lua
+++ lua/dawn/default_plugins/lsp.lua
@@ -2,7 +2,6 @@
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'diagnostic prev' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'diagnostic next' })
 
---vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'diagnostic setloclist' })
---vim.keymap.set('n', '<leader>lma', vim.diagnostic.open_float, { desc = 'diagnostic open float' })
+-- deprecated use default keymap gl
 lvim.builtin.which_key.mappings['lo'] =
 { '<cmd>lua vim.diagnostic.open_float()<cr>', 'diagnostic open open_float' }
