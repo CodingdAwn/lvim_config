721506662585f5310aa85f36b765b134bdd65137
dawn_ <gaoxifengfordawn@gmail.com>
2023-03-08 15:52:37 +0800

    取消lazygit的快捷键，使用默认的leader gg,之前的方法导致了在terminal下使用空格有延迟了

--- lua/dawn/default_plugins/terminal.lua
+++ lua/dawn/default_plugins/terminal.lua
@@ -1,3 +1,3 @@
 --print('load terminal config')
 -- <leader>gg also toggle a lazygit
-lvim.builtin.terminal.execs[#lvim.builtin.terminal.execs + 1] = { "lazygit", "<leader>tl", "lazygit terminal" }
+--lvim.builtin.terminal.execs[#lvim.builtin.terminal.execs + 1] = { "lazygit", "<leader>tl", "lazygit terminal" }
